86c1b993bcce502f5e324bed157b114b008a70f2
feixia5320 <1044489176@qq.com>
2020-01-15 18:38:34 +0800

    引入elementui、vuex、axios

--- shanshuiyuan/src/main.js
+++ shanshuiyuan/src/main.js
@@ -3,12 +3,36 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+import axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(Vuex);
+Vue.use(ElementUI);
 Vue.config.productionTip = false
+Vue.config.productionTip = false
+
+
+const store = new Vuex.Store({
+  state: {
+    nickName:'',
+    cartCount:0
+  },
+  mutations: {
+    updateUserInfo(state, nickName) {
+      state.nickName = nickName;
+    },
+    updateCartCount(state,cartCount){
+      state.cartCount += cartCount;
+    }
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
