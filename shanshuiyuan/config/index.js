a728aa9ad44b48f83fe38b8ac7830baa8ef758d4
feixia5320 <1044489176@qq.com>
2020-01-15 18:42:33 +0800

    代理配置

--- shanshuiyuan/config/index.js
+++ shanshuiyuan/config/index.js
@@ -10,12 +10,22 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/home':{
+          target:'http://localhost:3000'
+      },
+      '/news/*':{
+        target:'http://localhost:3000'
+      },
+      '/admin/*':{
+        target:'http://localhost:3000'
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    port: 8980, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
@@ -37,6 +47,7 @@ module.exports = {
   },
 
   build: {
+    port: 8989,
     // Template for index.html
     index: path.resolve(__dirname, '../dist/index.html'),
 
